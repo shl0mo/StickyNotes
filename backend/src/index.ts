9646e86c4a24931f0b5589da62fe517afd7b7d9c
shl0mo <salomao.dacostacruz@gmail.com>
2023-10-05 17:50:30 -0400

    back - dotenv, fs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World")
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const PORT = process.env.PORT || 7777
+
+const app = express()
+
+app.post("/login", (req: Request, res: Response) => {
+	
+})
