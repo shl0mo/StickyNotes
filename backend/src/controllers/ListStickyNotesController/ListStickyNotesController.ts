eead4e5f444c0b1555d2e3649ba002709f5d0387
shl0mo <salomao.dacostacruz@gmail.com>
2023-10-07 16:44:54 -0400

    listar cards #

--- backend/src/controllers/ListStickyNotesController/ListStickyNotesController.ts
+++ backend/src/controllers/ListStickyNotesController/ListStickyNotesController.ts
@@ -11,7 +11,7 @@ export const ListStickyNotesController = (req : Request, res : Response) : void 
 	const file_path = `./src/database/${CARDS_DOCUMENT}`
 	fs.readFile(file_path, 'utf-8', (err, data) => {
 		if (err) throw err
-		// const username : string = fs.readFileSync('sessao.txt', 'utf-8')
+		const username : string = fs.readFileSync('sessao.txt', 'utf-8')
 		const data_objects_array = JSON.parse(data)
 		const filtered_data = _.filter(data_objects_array, { "user": username })
 		// console.log(filtered_data)
